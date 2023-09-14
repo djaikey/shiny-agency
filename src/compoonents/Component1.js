4c1429f6caf8756f619a0e4bc39f4747e9c113c0
Johanna Keïta <keitajohanna@gmail.com>
2023-09-14 18:34:27 +0200

    Shiny-agency structure

---
+++ src/compoonents/Component1.js
@@ -0,0 +1,10 @@
+function Component1() {
+  //state
+
+  //comportements
+
+  //affichage
+  return <h1>Titre</h1>;
+}
+
+export default Component1;
---
+++ src/pages/Home/App.js
@@ -0,0 +1,11 @@
+import Component1 from "../../compoonents/Component1";
+import "../styles/App.css";
+function App() {
+  return (
+    <div className="App">
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
